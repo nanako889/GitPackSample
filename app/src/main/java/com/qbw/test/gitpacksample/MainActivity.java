5d21c91966d56e992f41f88b852af1a0104837c1
qbwjly <648026197@qq.com>
2016-08-21 19:27:06 +0800

    1

--- app/src/main/java/com/qbw/test/gitpacksample/MainActivity.java
+++ app/src/main/java/com/qbw/test/gitpacksample/MainActivity.java
@@ -3,11 +3,14 @@ package com.qbw.test.gitpacksample;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.qbw.test.androidlib.Show;
+
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Show.show();
     }
 }
